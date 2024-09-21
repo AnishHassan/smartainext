5546e20a8aafbc402185ad52f4e183fa2d7b0879
Anish Hassan <anish.hassanworks@gmail.com>
2024-09-22 02:05:58 +0500

    next-auth-feature

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,17 +6,16 @@ import { useEffect, useState } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Navbar = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
-
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     }, [])
     return (
         <nav className='flex-between w-full mb-16 pt-3 '>
@@ -27,11 +26,11 @@ const Navbar = () => {
             </Link>
             {/* Desktop Navigation */}
             <div className='sm:flex hidden'>
-                {isUserLoggedIn ? (<div className='flex gap-3 md:gap-5'>
+                {session?.user ? (<div className='flex gap-3 md:gap-5'>
                     <Link href='/create-prompt' className='black_btn'>Create Post</Link>
                     <button type='button' onClick={signOut} className='outline_btn'> Sign Out</button>
                     <Link href='/profile'>
-                        <Image src='/assets/images/logo.svg' className='rounded-full'
+                        <Image src={session?.user.image} className='rounded-full'
                             alt='profile' width={37} height={37} />
                     </Link>
                 </div>) : (<>
@@ -45,9 +44,26 @@ const Navbar = () => {
 
             {/* Mobile Navigation */}
             <div className='sm:hidden flex relative'>
-                {isUserLoggedIn ? (<div className='flex'>
-                    <Image src='/assets/images/logo.svg' className='rounded-full'
+                {session?.user ? (<div className='flex'>
+                    <Image src={session?.user.image} className='rounded-full'
                         alt='profile' width={37} height={37} onClick={() => setToggleDropdown((prev) => !prev)} />
+
+                    {toggleDropdown && (
+                        <div className='dropdown'>
+                            <Link href='/profile' className='dropdown_link' onClick={() => setToggleDropdown(false)}>
+                                My Profile
+                            </Link>
+                            <Link href='/create-prompt' className='dropdown_link' onClick={() => setToggleDropdown(false)}>
+                                Create Prompt
+                            </Link>
+                            <button type='button' className='mt-5 black_btn w-full' onClick={() => {
+                                setToggleDropdown(false);
+                                signOut();
+                            }}>
+                                Sign Out
+                            </button>
+                        </div>
+                    )}
                 </div>) : <>
                     {providers && Object.values(providers).map((provider) => (
                         <button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>
