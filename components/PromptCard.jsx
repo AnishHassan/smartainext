cd9cd780a2cd22a2f83a2894eb43c4da458558aa
AnishHassan <anish.hassanworks@gmail.com>
2024-10-13 16:23:53 +0500

    card css fix

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,63 +18,70 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
 
   return (
-    <div className="prompt_card flex flex-col justify-between h-full">
-      <div className="flex justify-between gap-5 items-start">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image
-            src={post.creator.image}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
-          <div className="flex flex-col">
-            <h3 className="font-satoshi semi-bold text-gray-900 ">
-              {post.creator.username}
-            </h3>
-            <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+    <>
+      <div className="prompt_card flex flex-col justify-between h-full">
+        <div className="flex justify-between gap-5 items-start">
+          <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+            <Image
+              src={post.creator.image}
+              alt="user_image"
+              width={40}
+              height={40}
+              className="rounded-full object-contain"
+            />
+            <div className="flex flex-col">
+              <h3 className="font-satoshi semi-bold text-gray-900 ">
+                {post.creator.username}
+              </h3>
+              <p className="font-inter text-sm text-gray-500">
+                {post.creator.email}
+              </p>
+            </div>
+          </div>
+
+          <div className="copy_btn" onClick={handleCopy}>
+            <Image
+              src={
+                copied === post.prompt
+                  ? "assets/icons/tick.svg"
+                  : "assets/icons/copy.svg"
+              }
+              width={12}
+              height={12}
+              alt="action-btn"
+            />
+          </div>
+        </div>
+        <p className="my-4 font-staoshi text-sm text-gray-700  flex-grow ">
+          {post.prompt}
+        </p>
+        <p
+          className="font-inter text-sm blue_gradient cursor_pointer"
+          onClick={() => handleTagClick && handleTagClick(post.tag)}
+        >
+          {post.tag}
+        </p>
+      </div>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="prompt_card_btns">
+          <div className="flex-center gap-4 border-gray-100  ">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
             </p>
           </div>
         </div>
-
-        <div className="copy_btn" onClick={handleCopy}>
-          <Image
-            src={
-              copied === post.prompt
-                ? "assets/icons/tick.svg"
-                : "assets/icons/copy.svg"
-            }
-            width={12}
-            height={12}
-            alt='action-btn'
-          />
-        </div>
-      </div>
-      <p className="my-4 font-staoshi text-sm text-gray-700  flex-grow">{post.prompt}</p>
-      <p
-        className="font-inter text-sm blue_gradient cursor_pointer"
-        onClick={() => handleClick && handleTagClick(post.tag)}
-      >
-        #{post.tag}
-      </p>
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
-        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3 ">
-          <p
-            className="font-inter text-sm green_gradient cursor-pointer"
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
-        </div>
       )}
-    </div>
+    </>
   );
 };
 
